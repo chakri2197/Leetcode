b7c542507f6de53b31bffc0af28f4a83e5138cbe
Balaga <cbalaga@ttu.edu>
2024-06-20 16:57:48 -0500

    3 changes

--- Sliding Window/3.Longest Substring Without Repeating Characters.cpp
+++ Sliding Window/3.Longest Substring Without Repeating Characters.cpp
@@ -4,20 +4,22 @@ public:
         
         unordered_map<char,int> map;
         int n=s.size();
-        for(char c: s){
-            map[c]++;
-        }
+        // for(char c: s){
+        //     map[c]++;
+        // }
         int l=0,r=0;
         int maxlen=-1;
         while(r<n){
             while(map.find(s[r])!=map.end() and l<=r){
-                cout<< "in erase"<<s[l]<<endl;
+                cout<< "in erase : "<<s[l]<<endl;
                 map.erase(s[l]);
                 l++;
             }
             if(map.find(s[r])==map.end()){
+                map[s[r]]++;
                 r++;
                 maxlen=max(maxlen,r-l+1);
+                cout<< "len : " << maxlen <<endl;
             }
             maxlen=max(maxlen,r-l+1);
         }
